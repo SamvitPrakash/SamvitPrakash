a038e7ac7731f8e945e59c6df58047c66ae613f6
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-13 00:19:36 +0200

    Small Fix to Accidental Import

--- ViteFolio/src/main.jsx
+++ ViteFolio/src/main.jsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
 import Home from './pages/Home_Page/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<App />} />
-      </Routes>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
