8485b9236ba7950e57ac2bcb8bc695d50d8fa6ef
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-13 00:21:49 +0200

    Refactor navigation links order and update hover styles for better UX

--- ViteFolio/src/components/Navigation_Bar/Navigation.jsx
+++ ViteFolio/src/components/Navigation_Bar/Navigation.jsx
@@ -29,9 +29,9 @@ const Navigation = () => {
 			<div className='nav-links'>
 				<a href="/">Home</a>
 				<a href="#projects">Projects</a>
-				<a id="contact" href="#contact">Contact Me</a>
+				<a href="#about">What's New</a>
+				<a href="#contact">Contact Me</a>
 				<ThemeButton isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-
 			</div>
 		</nav>
 
