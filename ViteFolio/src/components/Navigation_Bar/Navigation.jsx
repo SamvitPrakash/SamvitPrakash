68f2050ebdf8fc6a2c39bf4e99e972c4ff6b61ff
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-12 22:34:09 +0200

    Created a NavBar component

---
+++ ViteFolio/src/components/Navigation_Bar/Navigation.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './Navigation.css';
+import ThemeButton from '../Theme_Button/Theme_Button.jsx';
+
+const Navigation = () => {
+	const [isDarkMode, setIsDarkMode] = useState(false);
+
+	const toggleDarkMode = () => {
+		setIsDarkMode(!isDarkMode);
+		if (!isDarkMode) {
+			document.documentElement.style.setProperty('--current-text', 'var(--dark-text)');
+			document.documentElement.style.setProperty('--current-background', 'var(--dark-background)');
+			document.documentElement.style.setProperty('--current-primary', 'var(--dark-primary)');
+			document.documentElement.style.setProperty('--current-secondary', 'var(--dark-secondary)');
+			document.documentElement.style.setProperty('--current-accent', 'var(--dark-accent)');
+		} else {
+			document.documentElement.style.setProperty('--current-text', 'var(--light-text)');
+			document.documentElement.style.setProperty('--current-background', 'var(--light-background)');
+			document.documentElement.style.setProperty('--current-primary', 'var(--light-primary)');
+			document.documentElement.style.setProperty('--current-secondary', 'var(--light-secondary)');
+			document.documentElement.style.setProperty('--current-accent', 'var(--light-accent)');
+		}
+	}
+
+	return (
+
+		<nav className="navigation">
+			<h4 className="logo">My Portfolio</h4>
+			<div className='nav-links'>
+				<a href="/">Home</a>
+				<a href="#projects">Projects</a>
+				<a id="contact" href="#contact">Contact Me</a>
+				<ThemeButton isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+
+			</div>
+		</nav>
+
+	)
+};
+
+export default Navigation;
