7bc35b9f8c4e86141e8ae1a8627ab4775c806005
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-13 19:12:25 +0200

    Add EducationCard and HoneycombBackground components to Home page with updated styles

--- ViteFolio/src/pages/Home_Page/Home.jsx
+++ ViteFolio/src/pages/Home_Page/Home.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import Navigation from "../../components/Navigation_Bar/Navigation.jsx";
 import TypedName from "../../components/Typed_Name/Typed_Name.jsx";
+import EducationCard from "../../components/Education_Card/Education_Card.jsx";
+import HoneycombBackground from "../../components/Honeycomb_Background/Honeycomb_Background.jsx";
 import './Home.css'
 
 function Home() {
 	const [count, setCount] = useState(0);
+
+	const visibilityMatrix = [
+			[1,1,0,0,0,0,0,0,0,0,1],
+			[1,0,1,0,1,0,1,1,0,1,1],
+			[0,1,0,1,1,1,0,1,1,1,0],
+			[0,1,1,1,1,1,1,1,1,1,0],
+			[1,1,1,1,1,1,0,1,1,1,1],
+			[0,1,1,1,0,1,1,1,1,1,1],
+			[1,1,0,1,0,1,0,1,1,1,0],
+			[1,1,1,1,1,1,1,1,1,1,0],
+			[0,1,1,1,0,1,1,1,0,1,0],
+			[1,1,0,1,0,0,1,0,1,1,1]
+		];
 
 	return (
 		<>
@@ -13,6 +28,9 @@ function Home() {
 				<TypedName/>
 				<p className="bio">I am a final-year <em>BSc Computer Science</em> student at the <em>University of Pretoria</em>. As a passionate <em>back-end developer</em>, I have a strong interest in <em>software engineering</em> and a deep love for <em>problem-solving</em>. I truly enjoy the challenge of <em>designing</em> and <em>building</em> elegant and efficient solutions. My enthusiasm for technology extends beyond the code, as I also enjoy <em>discovering</em> and <em>experimenting</em> with new <em>Linux</em> distributions. I am currently using <em>Arch Linux</em> and has finnished exploring <em>Ubuntu</em> and various <em>Garuda</em> distributions. I am also deeply interested in learning about <em>cyber security</em> and enjoy the intricacies of <em>low-level programming</em>, including <em>Assembly</em>. Above all, I see myself as a <em>designer</em>, <em>creator</em> and a <em>lifelong learner</em>, driven by the desire to <em>build new things</em> and continually expand my knowledge in the ever-evolving tech landscape.</p>
 			</div>
+			<HoneycombBackground width={11} height={10} visibilityMatrix={visibilityMatrix}>
+				<h1 className="education-title"><span className="name">Educational</span> Background.</h1>
+			</HoneycombBackground>
 
 		</>
 	);
