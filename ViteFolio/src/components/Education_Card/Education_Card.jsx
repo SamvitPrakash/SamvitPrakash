8d907e68c047c677168793fde92f02a219492d3a
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-13 19:12:17 +0200

    Add EducationCard component with associated CSS for styling

---
+++ ViteFolio/src/components/Education_Card/Education_Card.jsx
@@ -0,0 +1,17 @@
+import './Education_Card.css';
+
+function EducationCard(){
+    return(
+        <div className="education-card">
+            <h2>Education</h2>
+            <div className="education-item">
+                <h3>BSc Computer Science</h3>
+                <p>University of Pretoria</p>
+                <p>2023 - Present</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default EducationCard;
