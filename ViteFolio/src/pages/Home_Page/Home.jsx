96faba01c7aa892ab328dbe3cec26f106febf8d9
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-13 22:50:22 +0200

    Update Home component structure and styles: adjust margin in CSS and change h1 to h2 for educational background

--- ViteFolio/src/pages/Home_Page/Home.jsx
+++ ViteFolio/src/pages/Home_Page/Home.jsx
@@ -9,16 +9,16 @@ function Home() {
 	const [count, setCount] = useState(0);
 
 	const visibilityMatrix = [
-			[1,1,0,0,0,0,0,0,0,0,1],
+			[1,1,0,0,0,0,0,0,0,1,1],
 			[1,0,1,0,1,0,1,1,0,1,1],
 			[0,1,0,1,1,1,0,1,1,1,0],
 			[0,1,1,1,1,1,1,1,1,1,0],
 			[1,1,1,1,1,1,0,1,1,1,1],
 			[0,1,1,1,0,1,1,1,1,1,1],
 			[1,1,0,1,0,1,0,1,1,1,0],
-			[1,1,1,1,1,1,1,1,1,1,0],
-			[0,1,1,1,0,1,1,1,0,1,0],
-			[1,1,0,1,0,0,1,0,1,1,1]
+			[0,1,0,1,1,1,1,1,1,1,0],
+			[0,0,0,0,0,1,1,1,0,1,0],
+			[0,0,0,0,0,0,1,0,1,1,1]
 		];
 
 	return (
@@ -29,7 +29,8 @@ function Home() {
 				<p className="bio">I am a final-year <em>BSc Computer Science</em> student at the <em>University of Pretoria</em>. As a passionate <em>back-end developer</em>, I have a strong interest in <em>software engineering</em> and a deep love for <em>problem-solving</em>. I truly enjoy the challenge of <em>designing</em> and <em>building</em> elegant and efficient solutions. My enthusiasm for technology extends beyond the code, as I also enjoy <em>discovering</em> and <em>experimenting</em> with new <em>Linux</em> distributions. I am currently using <em>Arch Linux</em> and has finnished exploring <em>Ubuntu</em> and various <em>Garuda</em> distributions. I am also deeply interested in learning about <em>cyber security</em> and enjoy the intricacies of <em>low-level programming</em>, including <em>Assembly</em>. Above all, I see myself as a <em>designer</em>, <em>creator</em> and a <em>lifelong learner</em>, driven by the desire to <em>build new things</em> and continually expand my knowledge in the ever-evolving tech landscape.</p>
 			</div>
 			<HoneycombBackground width={11} height={10} visibilityMatrix={visibilityMatrix}>
-				<h1 className="education-title"><span className="name">Educational</span> Background.</h1>
+				<h2 className="education-title"><span className="name">Educational</span> Background.</h2>
+				{/* <EducationCard /> */}
 			</HoneycombBackground>
 
 		</>
