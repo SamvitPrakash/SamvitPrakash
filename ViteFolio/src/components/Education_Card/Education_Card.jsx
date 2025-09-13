c909e18d85914096a110580c39daa6e765b7594e
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-14 01:42:00 +0200

    Refactor EducationCard component: update structure and styles, enhance responsiveness, and integrate react-icons for improved UI

--- ViteFolio/src/components/Education_Card/Education_Card.jsx
+++ ViteFolio/src/components/Education_Card/Education_Card.jsx
@@ -1,17 +1,29 @@
 import './Education_Card.css';
+import { FaRegCalendarAlt, FaLocationArrow } from "react-icons/fa";
 
-function EducationCard(){
+function EducationCard({icon, institution, degree, startYear, endYear, description, degreeLink}){
     return(
         <div className="education-card">
-            <img src='../../assets/Theme_Button/dark_mode.svg'></img>
-            <div className="education-item">
-                <h4>BSc Computer Science</h4>
-                <h5>University of Pretoria</h5>
-                <p>2023 - Present</p>
+            <div className="education-header">
+                <img className='education-logo' src={icon} alt={`${institution} Logo`} />
+                <div className='education-header-text'>
+                    <h4 className='education-institution'>{institution}</h4>
+                    <p className='education-institution'>
+                        <FaRegCalendarAlt className="year-icon" style={{marginRight: "0.5em"}} />
+                        {startYear}-{endYear}
+                    </p>
+                    
+                </div>
             </div>
+            <div className="education-body">
+                <h5 className='education-degree'>{degree}</h5>
+                <p>{description}</p>
+            </div>
+            <a className='view-degree-button' href={degreeLink} >Go to Institution
+                <FaLocationArrow className="degree-icon" style={{marginLeft: "0.5em"}} />
+            </a>
         </div>
-    )
-
+    );
 }
 
 export default EducationCard;
